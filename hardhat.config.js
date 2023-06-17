affe4bec886228b3e604e9df29b30d305948cbf1
Ashish Khatri <khatri8.ashish@gmail.com>
2023-06-18 00:44:01 +0530

    Add networks

--- hardhat.config.js
+++ hardhat.config.js
@@ -19,26 +19,30 @@ module.exports = {
   },
   networks: {
     polygonMumbai: {
-      url: process.env.POLYGON_MUMBAI_RPC_URL,
-      accounts: [process.env.PRIVATE_KEY]
+      url: process.env.POLYGON_MUMBAI_RPC_URL
     },
     polygon: {
-      url: process.env.POLYGON_RPC_URL,
-      accounts: [process.env.PRIVATE_KEY]
+      url: process.env.POLYGON_RPC_URL
     },
     fvm_calibration: {
-      url: process.env.FVM_CALIBRATION_RPC_URL,
-      accounts: [process.env.PRIVATE_KEY]
+      url: process.env.FVM_CALIBRATION_RPC_URL
     },
     fvm_mainnet: {
-      url: process.env.FVM_MAINNET_RPC_URL,
-      accounts: [process.env.PRIVATE_KEY]
+      url: process.env.FVM_MAINNET_RPC_URL
     },
+    ethereum: {
+      url: process.env.ETHEREUM_RPC_URL
+    },
+    goerli: {
+      url: process.env.GOERLI_RPC_URL
+    }
   },
   etherscan: {
     apiKey: {
       polygonMumbai: process.env.API_KEY_POLYGONSCAN,
       polygon: process.env.API_KEY_POLYGONSCAN,
+      mainnet: process.env.API_KEY_ETHERSCAN,
+      goerli: process.env.API_KEY_ETHERSCAN,
     }
   },
   contractSizer: {
