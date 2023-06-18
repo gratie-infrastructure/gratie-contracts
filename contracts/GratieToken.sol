5e7612f5de6cc1a1d184f8a51e00a0ac123cdee7
Ashish Khatri <khatri8.ashish@gmail.com>
2023-06-18 16:22:57 +0530

    Add claim function

--- contracts/GratieToken.sol
+++ contracts/GratieToken.sol
@@ -8,6 +8,9 @@ import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 contract GratieToken is ERC20Upgradeable, OwnableUpgradeable {
 
     string public iconURI;
+    uint256 public claimAmount;
+    uint256 public claimLimit;
+    mapping(address => uint256) public claims;
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
@@ -18,12 +21,15 @@ contract GratieToken is ERC20Upgradeable, OwnableUpgradeable {
         string memory _name,
         string memory _symbol,
         string memory _iconURI,
-        uint256 _supply
+        uint256 _supply,
+        uint256 _claimAmount
     ) external initializer {
         iconURI = _iconURI;
         __Ownable_init();
         __ERC20_init(_name, _symbol);
-        _mint(msg.sender, _supply);
+        _mint(msg.sender, _supply - _claimAmount);
+        _mint(address(this), _claimAmount);
+        claimAmount = _claimAmount;
     }
 
     function mint(
@@ -35,5 +41,18 @@ contract GratieToken is ERC20Upgradeable, OwnableUpgradeable {
 
     function burn(uint256 _amount) external {
         _burn(msg.sender, _amount);
+    }
+
+    function claim() external {
+        require(
+            claims[msg.sender] <= claimLimit,
+            "Claim Limit reached!"
+        );
+        ++claims[msg.sender];
+        transfer(msg.sender, claimAmount);
+    }
+
+    function updateClaimLimit(uint256 _claimLimit) external onlyOwner {
+        claimLimit = _claimLimit;
     }
 }
