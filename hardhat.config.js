5aea1b99cd7bdaf9d88f77796849990cec92ddf4
Ashish Khatri <khatri8.ashish@gmail.com>
2023-06-29 20:23:43 +0530

    Updated network configs

--- hardhat.config.js
+++ hardhat.config.js
@@ -24,17 +24,17 @@ module.exports = {
     polygon: {
       url: process.env.POLYGON_RPC_URL
     },
-    fvm_calibration: {
-      url: process.env.FVM_CALIBRATION_RPC_URL
-    },
-    fvm_mainnet: {
-      url: process.env.FVM_MAINNET_RPC_URL
-    },
     ethereum: {
       url: process.env.ETHEREUM_RPC_URL
     },
     goerli: {
       url: process.env.GOERLI_RPC_URL
+    },
+    fantom_opera: {
+      url: process.env.FANTOM_OPERA_RPC_URL
+    },
+    fantom_testnet: {
+      url: process.env.FANTOM_TESTNET_RPC_URL
     }
   },
   etherscan: {
@@ -43,6 +43,8 @@ module.exports = {
       polygon: process.env.API_KEY_POLYGONSCAN,
       mainnet: process.env.API_KEY_ETHERSCAN,
       goerli: process.env.API_KEY_ETHERSCAN,
+      opera: process.env.API_KEY_FANTOMSCAN,
+      ftmTestnet: process.env.API_KEY_FANTOMSCAN,
     }
   },
   contractSizer: {
